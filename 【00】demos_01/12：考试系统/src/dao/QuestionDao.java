2ae1da68499a962d50b52b65aabfa3fb0707285b
张国豪 <847818218@qq.com>
2020-07-22 11:19:50 +0800

    Completing：模拟考试系统 - a patch

--- 【00】demos_01/12：考试系统/src/dao/QuestionDao.java
+++ 【00】demos_01/12：考试系统/src/dao/QuestionDao.java
@@ -9,7 +9,7 @@ import java.util.Random;
 
 public class QuestionDao {
 
-    //负责读文件 随机生成一套试卷 题库10道题目 生成试卷是5道
+    //负责读文件 随机生成一套试卷 题库13道题目 生成试卷是5道
     //参数: 试卷题目数
     //返回值:ArrayList<Question>
     public ArrayList<Question> getPaper(int count){
@@ -18,7 +18,7 @@ public class QuestionDao {
         //题库
         ArrayList<Question> questionBank = new ArrayList<>(QuestionFileReader.getQuestion());
         while(paper.size() != 5){
-            int index = new Random().nextInt(questionBank.size());//[0, 10)
+            int index = new Random().nextInt(questionBank.size());//[0, 13)
             paper.add(questionBank.get(index));
         }
         return new ArrayList<>(paper);
