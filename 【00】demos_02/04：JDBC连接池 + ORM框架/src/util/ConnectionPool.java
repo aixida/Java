737ff1f9c041de98ff40966abff50eb6a40d18ae
张国豪 <847818218@qq.com>
2020-07-23 14:39:52 +0800

    Completing：JDBC连接池 + ORM框架 - 模拟JDBC连接池

---
+++ 【00】demos_02/04：JDBC连接池 + ORM框架/src/util/ConnectionPool.java
@@ -0,0 +1,100 @@
+package util;
+
+public class ConnectionPool {
+
+    private static final int BUSY_VALUE = 1;
+    private static final int FREE_VALUE = 0;
+    private static final int NULL_VALUE = -1;
+
+    //连接池
+    private static MyConnection[] connectionList = new MyConnection[5];
+    //连接池对应状态   占用1/释放0/空置-1
+    private static byte[] connectionBitMap = new byte[5];
+    //连接池存量
+    private static int total = 0;
+
+    //连接池初始化 每一个connection的状态空置
+    static {
+        for(byte c: connectionBitMap){
+            c = NULL_VALUE;
+        }
+    }
+
+    //连接池中获取connection
+    public static synchronized MyConnection getMyConnection(){
+        int freeIndex = getFreeIndex();
+        if(freeIndex > -1){
+            return distrute(freeIndex);
+        }
+        if(total < 10){//允许扩容
+            int nullIndex = getNullIndex();
+            if(nullIndex == -1){//需要扩容
+                nullIndex = grow();
+            }
+            return distrute(nullIndex);
+        }
+        return null;//连接池繁忙，没空理你
+    }
+
+    //负责在连接池中寻找已经释放的connection
+    private static int getFreeIndex(){
+        for (int i = 0; i < connectionBitMap.length; i++){
+            if(connectionBitMap[i] == FREE_VALUE){
+                return i;
+            }
+        }
+        return -1;//无已释放的connection
+    }
+
+    //负责在连接池中寻找空置的connection
+    private static int getNullIndex(){
+        for (int i = 0; i < connectionBitMap.length; i++){
+            if(connectionBitMap[i] == NULL_VALUE){
+                return i;
+            }
+        }
+        return -1;//无空置的connection
+    }
+
+    //分配connection
+    private static MyConnection distrute(int index){
+        if (connectionBitMap[index] == BUSY_VALUE){//占用中
+            return null;
+        }
+        MyConnection myConnection = null;
+        if (connectionBitMap[index] == NULL_VALUE){//空置
+            myConnection = new MyConnection(index);
+            connectionList[index] = myConnection;
+        } else if(connectionBitMap[index] == FREE_VALUE){//有connection但是处于释放状态
+            myConnection = connectionList[index];
+        }
+        connectionBitMap[index] = BUSY_VALUE;
+        total++;
+        return myConnection;
+    }
+
+    //扩容
+    private static int grow(){
+        //容量增倍
+        MyConnection[] newConnectionList = new MyConnection[connectionList.length * 2];
+        byte[] newConnectionBitMap = new byte[connectionBitMap.length * 2];
+        //复制并初始化
+        for (int i = 0; i < connectionList.length; i++){
+            newConnectionList[i] = connectionList[i];
+            newConnectionBitMap[i] = connectionBitMap[i];
+        }
+        int firstNullIndex = connectionList.length;
+        for (int i = firstNullIndex; i < newConnectionList.length; i++){
+            newConnectionBitMap[i] = NULL_VALUE;
+        }
+        connectionList = newConnectionList;
+        connectionBitMap = newConnectionBitMap;
+        return firstNullIndex;
+    }
+
+    //释放connection
+    public static void freeConnection(MyConnection myConnection){
+        connectionBitMap[myConnection.getIndex()] = FREE_VALUE;
+    }
+
+}
--- 【00】demos_02/04：JDBC连接池 + ORM框架/src/util/MyConnection.java
+++ 【00】demos_02/04：JDBC连接池 + ORM框架/src/util/MyConnection.java
@@ -1,16 +1,53 @@
 package util;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class MyConnection {
 
-    //将一个真实的连接和一个状态放在一起
-    private Connection connection;
-    private int index;
+    private Connection connection;//真实的连接
+    private int index;//索引
 
-    {
-            
+    public MyConnection(int index){
+        this.index = index;
     }
+
+    private static String driver = "com.mysql.cj.jdbc.Driver";
+    private static String url = "jdbc:mysql://localhost:3306/study?serverTimezone=CST";
+    private static String user = "root";
+    private static String password = "root";
+
+    //只加载驱动一次
+    static {
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //每次调用构造方法，块都会执行一遍（每次new出一个连接）
+    {
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
 
 
 }
