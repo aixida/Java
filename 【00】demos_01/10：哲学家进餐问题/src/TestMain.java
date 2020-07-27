7c2decc8e8d4eb5e7ac0e64ac47b2e522b7e964d
张国豪 <847818218@qq.com>
2020-07-27 16:11:42 +0800

    提交 demo 10：哲学家进餐问题 - 通过礼让解决该问题

--- 【00】demos_01/10：哲学家进餐问题/src/TestMain.java
+++ 【00】demos_01/10：哲学家进餐问题/src/TestMain.java
@@ -1,3 +1,8 @@
+/**
+ * 避免死锁的方法：
+ *  - 礼让----产生时间差（）使用这个方法
+ *  - 不要产生共用的问题
+ */
 public class TestMain {
 
     public static void main(String[] args){
@@ -8,10 +13,12 @@ public class TestMain {
         Chopstick c4 = new Chopstick(4);
 
         //四位哲学家
+        //  哲学家p1、p3坐在对桌, 可以同时拿起两双筷子
+        //  哲学家p2、p4坐在对桌, 同理
         Philosopher p1 = new Philosopher("哲学家a",c1,c2,0);
-        Philosopher p2 = new Philosopher("哲学家b",c2,c3,0);
+        Philosopher p2 = new Philosopher("哲学家b",c2,c3,1000);
         Philosopher p3 = new Philosopher("哲学家c",c3,c4,0);
-        Philosopher p4 = new Philosopher("哲学家d",c4,c1,0);
+        Philosopher p4 = new Philosopher("哲学家d",c4,c1,1000);
 
         p1.start();
         p2.start();
